52032c00dd587df4e141a4a416c07e578a1117d4
Ian Hill <ianhill@buymeasoda.com>
2011-04-15 23:10:09 +1000

    Add tiny-tmpl version and simply key lookup logic

--- tiny-tmpl.js
+++ tiny-tmpl.js
@@ -1,16 +1,11 @@
 
 function tmpl(template, data) {
     return template.replace(/\{\{([^}]+)\}\}/g, function (tag, key) {
-        var value;
-        if (key.match(/\./) && key !== ".") {
-            value = data;
-            key = key.split(".");
-            for (var i = 0, l = key.length; i < l; i++) {
-                value = value[key[i]];
-            }            
-        } else {
-            value = data[key];
-        }
+        var value = data, 
+            keys = key.split(".");
+        for (var i = 0, l = keys.length; i < l; i++) {
+            value = value[keys[i]];
+        }            
         return value || tag;
     });
 }
