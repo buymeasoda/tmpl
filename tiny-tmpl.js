46bef110a31c3b4daac84702df38e56f8a3ea1c8
Ian Hill <ianhill@buymeasoda.com>
2011-04-15 20:46:17 +1000

    Escaping { and } and removing errant semi-colon

--- tiny-tmpl.js
+++ tiny-tmpl.js
@@ -1,6 +1,6 @@
 
 function tmpl(template, data) {
-    return template.replace(/{{([^}]+)}}/g, function (str, val) {
+    return template.replace(/\{\{([^}]+)\}\}/g, function (str, val) {
         return data[val] || str;
     });
-};
+}
