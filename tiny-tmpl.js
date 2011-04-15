0ba8289e981363c4c2a91f154eedbf484b34dba9
Ian Hill <ianhill@buymeasoda.com>
2011-04-15 21:51:19 +1000

    Add implementation for retrieving values from nested object references

--- tiny-tmpl.js
+++ tiny-tmpl.js
@@ -1,6 +1,14 @@
 
+function getObjectValue(ref, obj) {
+    ref = ref.split(".");
+    for (var i = 0, l = ref.length; obj && i < l; i++) {
+        obj = obj[ref[i]] || undefined;
+    }
+    return obj;
+}
+
 function tmpl(template, data) {
-    return template.replace(/\{\{([^}]+)\}\}/g, function (str, val) {
-        return data[val] || str;
+    return template.replace(/\{\{([^}]+)\}\}/g, function (tag, ref) {
+        return getObjectValue(ref, data) || tag;
     });
 }
