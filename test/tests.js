f3581f9e7b58a25a4ab53b71a4d39d34a42dfc4c
Ian Hill <ianhill@buymeasoda.com>
2011-04-15 20:43:29 +1000

    Adding simple template test for basic object

--- test/tests.js
+++ test/tests.js
@@ -4,3 +4,7 @@ module("tmpl tests");
 test("tmpl exists", function() {
 	ok(tmpl);
 });
+
+test("tmpl replaces simple content", function () {
+    equals(tmpl("<p>Hello {{name}}</p>", {name: "world"}), "<p>Hello world</p>");
+});
